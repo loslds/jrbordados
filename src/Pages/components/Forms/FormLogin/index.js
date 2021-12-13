d3b2072da81df039e01b549d11a24c8f7c40dd51
loslds <los.sbrissa@gmail.com>
2021-12-13 19:20:36 -0300

    nv

--- src/Pages/components/Forms/FormLogin/index.js
+++ src/Pages/components/Forms/FormLogin/index.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import useForm from './hooks'
 import { Container } from './../staylesForms'
 
 export default function FormLogin() {
-  const [{ values, loading }, handleChange, handleSubmit] = useForm()
+  const [loading, setLoading] = useState(false)
+  const [handleChange, handleSubmit] = useForm()
 
-  const enviarContato = () => {
-    // faça o que for preciso :)
-    console.log(values)
+  const enviarContato = formData => {
+    // const enviarContato = async formData => {
+    console.log('estarei enviando...')
+    // const invalid = invalidateData(formData)
+    // if (!invalid) {
+    //   setLoading(true)
+    //   const response = await send(formData)
+    //   setLoading(false)
+    //   console.log(formData)
   }
 
   return (
